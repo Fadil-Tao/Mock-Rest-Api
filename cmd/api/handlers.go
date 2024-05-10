443fdf013e07e9bb732d09a839718f9b07909ac3
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-05-10 11:59:59 +0800

    little changes

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,3 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: Tersedia")
+	fmt.Fprintf(w, "environment: %s\n ", app.config.env )
+	fmt.Fprintf(w, "Version : %s\n", version)
+}
+
 
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const version = "1.0.0"
@@ -15,10 +18,37 @@ type config struct{
 
 type application struct {
 	config config
-	log log.Logger
+	log *log.Logger
 }
 
 func main() {
-	fmt.Println("Hello Api")
+	var cfg config
+
+	flag.IntVar(&cfg.port,"port",8080,"API SERVER PORT")
+	flag.StringVar(&cfg.env,"environtment","developement", "Environtment (development|staging|production)")
+	flag.Parse()
+
+	infolog := log.New(os.Stdout,"INFORMASI :", log.Ldate | log.Ltime)
+
+	app := &application{
+		config: cfg,
+		log: infolog,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.HealthCheckHandler)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d",cfg.port),
+		Handler: mux,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	infolog.Printf("Starting %s server on %s",cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	infolog.Fatal(err)
+
 
 }
