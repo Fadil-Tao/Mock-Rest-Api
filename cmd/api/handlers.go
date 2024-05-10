44d0d3751dfba028820fa1773372acdb84b87db8
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-05-10 16:50:28 +0800

    making few endpoint

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -11,4 +11,31 @@ func (app *application) HealthCheckHandler(w http.ResponseWriter, r *http.Reques
 	fmt.Fprintf(w, "Version : %s\n", version)
 }
 
+// user handlers
+func (app *application) CreateUser(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Creating user here"))
+} 
 
+func (app *application) AddTweet(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Creating post here"))
+}
+
+func (app *application) SeeTweets(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("You should be able see list of tweets here"))
+}
+
+func (app *application) DeleteTweet(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Deleting tweet here"))
+}
+
+func (app *application) UpdateTweet(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Updating tweet here"))
+}
+
+func (app *application) AddComment(w http.ResponseWriter , r *http.Request){
+	w.Write([]byte("Adding comment here"))
+}
+
+func (app *application) DeleteComment(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Creating user here"))
+}
--- cmd/api/router.go
+++ cmd/api/router.go
@@ -1,2 +1,18 @@
-package main 
+package main
 
+import "net/http"
+
+func (app *application) Router() http.Handler{
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthcheck", app.HealthCheckHandler)
+	mux.HandleFunc("GET /tweet/list" , app.SeeTweets)
+	mux.HandleFunc("POST /tweet/create", app.AddTweet)
+	mux.HandleFunc("PUT /tweet/edit/", app.UpdateTweet)
+	mux.HandleFunc("DELETE /tweet/delete/", app.DeleteTweet)
+	mux.HandleFunc("POST /tweet/comment/create", app.AddComment)
+
+	v1 := http.NewServeMux()
+	v1.Handle("/v1/" , http.StripPrefix("/v1", mux))
+
+	return v1	
+}
